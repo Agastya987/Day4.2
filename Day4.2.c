30c9c4e34de45201fda2f16b02d1ef0729b35e1d
Agastya987 <74403391+Agastya987@users.noreply.github.com>
2025-08-28 08:35:15 +0530

    Add files via upload

---
+++ Day4.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+    // Input n
+    scanf("%d", &n);
+
+    // Formula for sum of first n natural numbers
+    sum = n * (n + 1) / 2;
+
+    // Output
+    printf("Sum=%d\n", sum);
+
+    return 0;
+}
